d78f987852de7d81cee6bc719cad89e8066ddd56
edrakeford1 <edrakeford@student.cscc.edu>
2026-03-15 21:18:17 -0400

    init

---
+++ coin.py
@@ -0,0 +1,9 @@
+"""
+Program: Match Coins Game - Coin Class
+Name: Elijah Drakeford
+Purpose: Defines the Coin class, which represents a single coin
+that is tossed and keeps track of whether it lands on heads or tails.
+Starter Code: N/A
+Date: March 15, 2026
+"""
+
