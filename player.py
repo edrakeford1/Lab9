30078e79de7d5ee6883e461e0d8b2b97897b7a5d
edrakeford1 <edrakeford@student.cscc.edu>
2026-03-15 23:00:41 -0400

    player checkpoint

---
+++ player.py
@@ -0,0 +1,33 @@
+"""
+Program: Player Class
+Name: Elijah Drakeford
+Purpose: Defines the Player class. Each player has a name, wallet of coins, and a coin object to toss.
+Starter Code: None
+Date: March 15, 2026
+"""
+
+from coin import Coin
+
+class Player:
+    def __init__(self, name):
+        self.__name = name
+        self.__wallet = 20
+        self.__coin = Coin()
+
+    def toss_coin(self):
+        self.__coin.toss()
+
+    def get_coin_side(self):
+        return self.__coin.get_sideup()
+
+    def win_coin(self):
+        self.__wallet += 1
+
+    def lose_coin(self):
+        self.__wallet -= 1
+
+    def get_wallet(self):
+        return self.__wallet
+
+    def get_name(self):
+        return self.__name
