97eb11f906730fb269e5ceb271a373584a6e6764
edrakeford1 <edrakeford@student.cscc.edu>
2026-03-15 23:02:01 -0400

    main init

---
+++ Lab9_edrakeford-1.py
@@ -0,0 +1,10 @@
+"""
+Program: Match Coins Game
+Name: Elijah Drakeford
+Purpose: This program runs the Match Coins game using the Player
+and Coin classes. Two players toss coins and gain or lose coins
+based on the game rules.
+Starter Code: None
+Date: March 15, 2026
+"""
+
